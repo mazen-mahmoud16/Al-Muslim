42b136a514d0c1cc0b6d09b0b097fdaa1a1e0f57
Mazen Mahmoud <mazen.mahmoud.freelancer@gmail.com>
2022-11-30 22:32:14 +0200

    Adding Splash screen

---
+++ app/src/main/java/com/example/elaislami/Splash.java
@@ -0,0 +1,31 @@
+package com.example.elaislami;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
