fd9b55bcbee66aa0a3225a300582340a21551b40
Mazen Mahmoud <mazen.mahmoud.freelancer@gmail.com>
2022-12-13 04:53:58 +0200

    1) Implement Sibha 2) Make qualifiers to versions that support the clickable image button effect

--- app/src/main/java/com/example/elaislami/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/elaislami/Fragment/HomeFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,6 +26,9 @@ public class HomeFragment extends Fragment {
     private ImageView statistics_btn;
     private ImageView azkar_btn;
     private TextView loc;
+    private TextView sibhaCounter;
+    private ImageButton reset;
+    private ImageButton add;
 
 
     Intent intent;
@@ -41,9 +45,38 @@ public class HomeFragment extends Fragment {
         statistics_btn = view.findViewById(R.id.img2);
         azkar_btn = view.findViewById(R.id.img3);
 
+        sibhaCounter = view.findViewById(R.id.sibha);
+        reset = view.findViewById(R.id.reset);
+        add = view.findViewById(R.id.add);
+
+
         loc=view.findViewById(R.id.loc);
         SharedPreferences settings = getActivity().getSharedPreferences(PREFS_NAME, 0);
         loc.setText(settings.getString("address", "Loading"));
+
+        sibhaCounter.setText(String.valueOf(settings.getInt("sibha", 0)));
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putInt("sibha",0);
+                editor.commit();
+                sibhaCounter.setText(String.valueOf(0));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int curr_counter = Integer.parseInt(sibhaCounter.getText().toString());
+                curr_counter++;
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putInt("sibha",curr_counter);
+                editor.commit();
+                sibhaCounter.setText(String.valueOf(curr_counter));
+            }
+        });
 
 
         SharedPreferences.OnSharedPreferenceChangeListener sharedPreferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
