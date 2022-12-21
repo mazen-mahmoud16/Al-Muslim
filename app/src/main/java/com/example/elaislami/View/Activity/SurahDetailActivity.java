e0f4ee28f28c9d6c6bf2787b1d38958d3a1bea0d
Abouraya11 <youssefabouraya7@gmail.com>
2022-12-21 22:02:55 +0200

    1) Add surah audio functionality in SurahDetailActivity

--- app/src/main/java/com/example/elaislami/View/Activity/SurahDetailActivity.java
+++ app/src/main/java/com/example/elaislami/View/Activity/SurahDetailActivity.java
@@ -9,17 +9,28 @@ import com.example.elaislami.Adapter.AyahListAdapter;
 import com.example.elaislami.R;
 import com.example.elaislami.RoomDBManager.RoomDBModels.AyahDBModel;
 import com.example.elaislami.RoomDBManager.ViewModel.AyahViewModel;
-import com.example.elaislami.RoomDBManager.ViewModel.SurahViewModel;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import es.dmoral.toasty.Toasty;
 
 /*
  * Here is Surah details activity that triggers when the user clicks to a specific surah
@@ -27,11 +38,12 @@ import java.util.List;
 public class SurahDetailActivity extends AppCompatActivity {
 
     /*
-     * Edit texts, image buttons, recycler view and buttons used
+     * Edit texts, image buttons, recycler view , image button and buttons used
      */
     private RecyclerView rvSurahDetail;
-    private TextView tvEnglishName, tvArabicName, tvJuz;
+    private TextView tvEnglishName, tvArabicName, tvJuz, startTime , totalTime;
     private ImageView back_btn;
+    private ImageButton playButton;
 
     // Get the arabic and english name of surah
     private String englishName,arabicName;
@@ -55,6 +67,15 @@ public class SurahDetailActivity extends AppCompatActivity {
     // Adapter
     private AyahListAdapter ayahAdapter;
 
+    // Handler for surah audio
+    private Handler handler = new Handler();
+
+    // Surah audio seek bar
+    SeekBar seekBar;
+
+    // Media player reference
+    MediaPlayer mediaPlayer;
+
     /*
      * Here is on create function
      */
@@ -74,6 +95,10 @@ public class SurahDetailActivity extends AppCompatActivity {
         tvEnglishName =findViewById(R.id.SurahEnglishName);
         tvArabicName =findViewById(R.id.SurahArabicName);
         back_btn=findViewById(R.id.back_btn);
+        playButton = findViewById(R.id.play_button);
+        startTime = findViewById(R.id.start_time);
+        totalTime = findViewById(R.id.total_time);
+        seekBar = findViewById(R.id.seekBar);
 
         // Set toolbar
         setSupportActionBar(toolbar);
@@ -147,6 +172,198 @@ public class SurahDetailActivity extends AppCompatActivity {
             startActivity(i);
 
         });
+
+        /*
+         * Calling listen audio method to initiate and start playing the surah audio on click
+         */
+        try {
+            listenAudio();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * Checking if the device has any network connection
+     */
+    private boolean haveNetworkConnection() {
+
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return !haveConnectedWifi && !haveConnectedMobile;
+    }
+
+    /*
+     * listen audio method to initiate and start playing the surah audio on click and to handle on click of pause or play
+     */
+    @SuppressLint("ClickableViewAccessibility")
+    private void listenAudio() throws IOException {
+
+        mediaPlayer = new MediaPlayer();
+        seekBar.setMax(100);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mediaPlayer.isPlaying()){
+                    handler.removeCallbacks(updater);
+                    mediaPlayer.pause();
+                    playButton.setImageResource(R.drawable.ic_play_circle);
+                }else{
+                    if(haveNetworkConnection()){
+                        Toasty.error(SurahDetailActivity.this,"Please enable internet connection",Toasty.LENGTH_SHORT,true).show();
+                        return;
+                    }
+                    mediaPlayer.start();
+                    playButton.setImageResource(R.drawable.ic_pause_circle);
+                    updateSeekBar();
+                }
+            }
+        });
+
+        preparedMediaPlayer();
+
+        /*
+         * Fired when user clicks on seek bar to move forward or backward
+         */
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(haveNetworkConnection()){
+                    Toasty.error(SurahDetailActivity.this,"Please enable internet connection",Toasty.LENGTH_SHORT,true).show();
+                    return false;
+                }
+                SeekBar seekBar = (SeekBar) v;
+                int playPosition = (mediaPlayer.getDuration() / 100) * seekBar.getProgress();
+                mediaPlayer.seekTo(playPosition);
+                startTime.setText(timeToMilliSecond(mediaPlayer.getCurrentPosition()));
+                return false;
+            }
+        });
+
+        /*
+         * Fired when user drag the progress bar forward or backward
+         */
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                seekBar.setSecondaryProgress(percent);
+            }
+        });
+
+        /*
+         * Fired when the audio is finished
+         */
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+
+                seekBar.setProgress(0);
+                playButton.setImageResource(R.drawable.ic_play_circle);
+                startTime.setText("0:00");
+                totalTime.setText("0:00");
+                mediaPlayer.reset();
+                try {
+                    preparedMediaPlayer();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /*
+     * Fired to set data source from the website and get the surah audio from it to stream the surah audio got
+     */
+    private void preparedMediaPlayer() throws IOException {
+
+        if(haveNetworkConnection()){
+            Toasty.error(SurahDetailActivity.this,"Please enable internet connection",Toasty.LENGTH_SHORT,true).show();
+            return;
+        }
+
+        String surahNumber="";
+        if(surahIndex<10){
+            surahNumber = "00"+surahIndex;
+        }else if(surahIndex<100){
+            surahNumber = "0"+surahIndex;
+        }else if(surahIndex>=100){
+            surahNumber = String.valueOf(surahIndex);
+        }
+        //https://download.quranicaudio.com/quran/abdul_wadood_haneef_rare/001.mp3
+        mediaPlayer.setDataSource("https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/"+surahNumber.trim()+".mp3");
+        mediaPlayer.prepare();
+        totalTime.setText(timeToMilliSecond(mediaPlayer.getDuration()));
+    }
+
+    /*
+     * Start thread to update progress bar and update the counter text view beside the progress bar
+     */
+    private Runnable updater = new Runnable() {
+        @Override
+        public void run() {
+            updateSeekBar();
+            long currentDuration = mediaPlayer.getCurrentPosition();
+            startTime.setText(timeToMilliSecond(currentDuration));
+        }
+    };
+
+    /*
+     * Called when to update progress bar
+     */
+    private void updateSeekBar(){
+        if(mediaPlayer.isPlaying()){
+            seekBar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) *100));
+            handler.postDelayed(updater,1000);
+        }
+    }
+
+    /*
+     * Called to convert time to milli seconds
+     */
+    private String timeToMilliSecond(long milliSecond){
+        String timerString = "";
+        String secondString ;
+
+        int hours = (int) (milliSecond /(1000 * 60 * 60));
+        int minutes = (int) (milliSecond % (1000 * 60 * 60)) / (1000 * 60);
+        int second = (int) ((milliSecond % (1000 * 60 * 60))% (1000 * 60) /1000);
+
+        if(hours>0){
+            timerString = hours + ":";
+        }
+        if(second < 10){
+            secondString = "0" + second;
+        }else{
+            secondString = "" + second;
+        }
+        timerString = timerString + minutes + ":" + secondString;
+        return timerString;
+    }
+
+    /*
+     * Override on stop to stop playing the audio when user close the surah detail activity or close the application
+     */
+    @Override
+    protected void onStop() {
+        if(mediaPlayer.isPlaying()) {
+            handler.removeCallbacks(updater);
+            mediaPlayer.pause();
+            playButton.setImageResource(R.drawable.ic_play_circle);
+        }
+        super.onStop();
     }
 
 }
