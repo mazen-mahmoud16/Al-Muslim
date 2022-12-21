357ae586c6e7c4b926f2b0910c14f41d995f0856
Mazen Mahmoud <mazen.mahmoud.freelancer@gmail.com>
2022-12-21 02:38:10 +0200

    Clean up surah view model code

--- app/src/main/java/com/example/elaislami/ViewModel/SurahViewModel.java
+++ app/src/main/java/com/example/elaislami/ViewModel/SurahViewModel.java
@@ -1,7 +1,5 @@
 package com.example.elaislami.ViewModel;
 import android.app.Application;
-import android.content.SharedPreferences;
-import android.util.Log;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
@@ -15,20 +13,31 @@ import com.example.elaislami.RoomDBModels.TodoItemDBModel;
 
 import java.util.List;
 
+/*
+ * Here is the class Surah view model, to manage surah, ayah and todo models functions
+ */
 public class SurahViewModel extends AndroidViewModel {
 
-    private SurahListRepository sRepository;
-    private AyahListRepository aRepository;
-    private TodoListRepository tRepository;
-    private LiveData<List<SurahDBModel>> surahList;
-    private LiveData<List<AyahDBModel>> ayahList;
-    private LiveData<List<TodoItemDBModel>> todoItems;
+    // Repository used
+    private final TodoListRepository tRepository;
+
+    /*
+     * Livedata lists used
+     */
+    private final LiveData<List<SurahDBModel>> surahList;
+    private final LiveData<List<AyahDBModel>> ayahList;
+    private final LiveData<List<TodoItemDBModel>> todoItems;
 
 
+    // Public constructor
     public SurahViewModel(Application application) {
         super(application);
-        sRepository = new SurahListRepository(application);
-        aRepository = new AyahListRepository(application);
+
+        /*
+         * List of repositories used
+         */
+        SurahListRepository sRepository = new SurahListRepository(application);
+        AyahListRepository aRepository = new AyahListRepository(application);
         tRepository = new TodoListRepository(application);
         surahList = sRepository.getAllSurahs();
         ayahList = aRepository.getAllAyahs();
@@ -37,17 +46,25 @@ public class SurahViewModel extends AndroidViewModel {
         aRepository.getAllAyahsApi();
     }
 
+    // Here is the function that returns the live data of all Surahs
     public LiveData<List<SurahDBModel>> getAllSurahs() {
         return surahList;
     }
+
+    // Here is the function that returns the live data of all Ayahs
     public LiveData<List<AyahDBModel>> getAllAyahs() {
         return ayahList;
     }
+
+    // Here is the function that returns the live data of all Todo Items
     public LiveData<List<TodoItemDBModel>> getAllTodoList() {
         return todoItems;
     }
 
+    // Here is the function to insert a todo item
     public void insertTodoItem(TodoItemDBModel todoItem) { tRepository.insert(todoItem); }
+
+    // Here is the function to delete a todo item
     public void deleteTodoItem(int todoId) {
         tRepository.delete(todoId);
     }
